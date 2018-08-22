13f629be7c367137dd3e70ab5cbf8c75a8910d99
boolangery <eliott.dumeix@gmail.com>
2018-08-22 11:38:12 +0200

    fix json array of json:object.
    * allow the user to use the syntax:
      :property [myjsonobject] modules: desc

--- sphinxjsondomain.py
+++ sphinxjsondomain.py
@@ -14,6 +14,22 @@ except ImportError:
     yaml = None
 
 
+def is_json_array(typename):
+    """ Check if a property type is an array.
+        For example: [int], [uuid4] are array
+    """
+    return typename and typename.startswith('[') and typename.endswith(']')
+
+
+def strip_json_array(typename):
+    """ Strip json array enclosing brackets from a typename.
+        [int] -> int
+    """
+    if typename and typename.startswith('[') and typename.endswith(']'):
+        return typename[1:-1]
+    return typename
+
+
 class JsonField(docfields.TypedField):
     def __init__(self, name, names=(), typenames=[], label=None, examplenames=(),
                  rolename=None, typerolename=None, can_collapse=False):
@@ -31,30 +47,100 @@ class JsonFieldTransformer(docfields.DocFieldTransformer):
             for name in fieldtype.examplenames:
                 self.examplemap[name] = True
 
-    def transform_all(self, node):
+    def transform(self, node):
         # type: (nodes.Node) -> None
-        """Transform all field list children of a node."""
-        # don't traverse, only handle field lists that are immediate children
+        """Transform a single field list *node*."""
+        typemap = self.typemap
 
-        for child in node:
-            if isinstance(child, nodes.field_list):
+        entries = []
+        groupindices = {}  # type: Dict[unicode, int]
+        types = {}  # type: Dict[unicode, Dict]
 
-                # remove examplenames nodes
-                nodes_to_delete = []
-                for c in child.children:
-                    fieldname, fieldbody = c
-                    try:
-                        # split into field type and argument
-                        fieldtype, fieldarg = fieldname.astext().split(None, 1)
-                        if fieldtype in self.examplemap:
-                            nodes_to_delete.append(c)
-                    except ValueError:
-                        pass
+        # step 1: traverse all fields and collect field types and content
+        for field in node:
+            fieldname, fieldbody = field
+            try:
+                # split into field type and argument
+                fieldtype, fieldarg = fieldname.astext().split(None, 1)
+            except ValueError:
+                # maybe an argument-less field type?
+                fieldtype, fieldarg = fieldname.astext(), ''
+            typedesc, is_typefield = typemap.get(fieldtype, (None, None))
 
-                for n in nodes_to_delete:
-                    child.children.remove(n)
+            # collect the content, trying not to keep unnecessary paragraphs
+            if docfields._is_single_paragraph(fieldbody):
+                content = fieldbody.children[0].children
+            else:
+                content = fieldbody.children
 
-                self.transform(child)
+            # sort out unknown fields
+            if typedesc is None or typedesc.has_arg != bool(fieldarg):
+                continue
+
+            typename = typedesc.name
+
+            # if the field specifies a type, put it in the types collection
+            if is_typefield:
+                # filter out only inline nodes; others will result in invalid
+                # markup being written out
+                content = [n for n in content if isinstance(n, nodes.Inline) or
+                           isinstance(n, nodes.Text)]
+                if content:
+                    types.setdefault(typename, {})[fieldarg] = content
+                continue
+
+            # also support syntax like ``:param type name:``
+            if typedesc.is_typed:
+                try:
+                    argtype, argname = fieldarg.split(None, 1)
+                except ValueError:
+                    pass
+                else:
+                    xrefs = typedesc.make_xrefs(
+                        nodes.Text(argtype),
+                        self.directive.domain,
+                        nodes.Text(argtype),
+                    )
+                    xrefs[0].attributes['json:name'] = strip_json_array(nodes.Text(argtype))
+                    types.setdefault(typename, {})[argname] = xrefs
+                    fieldarg = argname
+
+            translatable_content = nodes.inline(fieldbody.rawsource,
+                                                translatable=True)
+            translatable_content.document = fieldbody.parent.document
+            translatable_content.source = fieldbody.parent.source
+            translatable_content.line = fieldbody.parent.line
+            translatable_content += content
+
+            # grouped entries need to be collected in one entry, while others
+            # get one entry per field
+            if typedesc.is_grouped:
+                if typename in groupindices:
+                    group = entries[groupindices[typename]]
+                else:
+                    groupindices[typename] = len(entries)
+                    group = [typedesc, []]
+                    entries.append(group)
+                entry = typedesc.make_entry(fieldarg, [translatable_content])
+                group[1].append(entry)
+            else:
+                entry = typedesc.make_entry(fieldarg, [translatable_content])
+                entries.append([typedesc, entry])
+
+        # step 2: all entries are collected, construct the new field list
+        new_list = nodes.field_list()
+        for entry in entries:
+            if isinstance(entry, nodes.field):
+                # pass-through old field
+                new_list += entry
+            else:
+                fieldtype, content = entry
+                fieldtypes = types.get(fieldtype.name, {})
+                env = self.directive.state.document.settings.env
+                new_list += fieldtype.make_field(fieldtypes, self.directive.domain,
+                                                 content, env=env)
+
+        node.replace_self(new_list)
 
 
 class JSONObject(directives.ObjectDescription):
@@ -351,13 +437,16 @@ class JSONDomain(domains.Domain):
         the type is ``uri``, then a link to the RFC is generated.
 
         """
+
         if node.get('json:name'):
+
             objdef = self.get_object(node['json:name'])
             if objdef:
                 return node_utils.make_refnode(builder, fromdocname,
                                                objdef.docname, objdef.key,
                                                contnode)
-        if typ == 'jsonprop':
+
+        if typ in self.REF_TYPES:
             try:
                 ref = nodes.reference(internal=False)
                 ref['refuri'], ref['reftitle'] = self.REF_TYPES[target]
@@ -490,8 +579,8 @@ class PropertyDefinition(object):
                             name = tokens[1]
 
                         # check if name is enclosed by []
-                        if typ and typ.startswith('[') and typ.endswith(']'):
-                            typ = typ[1:-1]
+                        if is_json_array(typ):
+                            typ = strip_json_array(typ)
 
                             self.property_qualifiers[name] = self.property_qualifiers.get(name, PropertyQualifier())
                             self.property_qualifiers[name].is_array = True
