0073a454342157eb88f3b780b55df5cf76522c03
boolangery <eliott.dumeix@gmail.com>
2018-08-29 09:07:33 +0200

    version 0.0.4

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ def read_requirements(name):
 
 setuptools.setup(
     name='sphinx-jsondomain',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/dave-shawley/sphinx-jsondomain',
     description='Describe JSON document structures in sphinx',
     long_description='\n'+open('README.rst').read(),
     license='BSD',
-    author='Dave Shawley',
-    author_email='daveshawley+python@gmail.com',
+    author='Dave Shawley, Eliott Dumeix',
+    author_email='eliott.dumeix@gmail.com',
     py_modules=['sphinxjsondomain'],
     install_requires=read_requirements('installation.txt'),
     classifiers=[
