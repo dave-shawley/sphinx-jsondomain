e9530dbac595d2675c1d07b960ab946a7ead0a15
Xander Harris <xander.harris@wunderkind.co>
2023-02-03 12:30:52 -0800

    Update test
    See #1

--- test_sphinxjsondomain.py
+++ test_sphinxjsondomain.py
@@ -2,4 +2,6 @@ import sphinxjsondomain
 
 def test_normalize_object_name():
     """Test object name normalization."""
-    assert True
+    previous_object_name = ':json:object:: test'
+    result = sphinxjsondomain.normalize_object_name(previous_object_name)
+    assert result == ':json:object::-test'
